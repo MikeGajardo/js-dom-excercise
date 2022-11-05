6524e751244fb79a1d5297c62b053ea039aef77e
Michael Gajardo <105249375+MikeGajardo@users.noreply.github.com>
2022-11-04 23:10:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var list = ul.getElementsByTagName("li")
+var listItem = document.querySelectorAll("li");
+
+console.log(list)
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+//Add Strikethrough to clicked li items
+
+function addDoneAfterClick(event) {
+	if (event.target === event.target) {
+		event.target.classList.toggle("done");
+	}
+} 
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+ul.addEventListener("click", addDoneAfterClick);
