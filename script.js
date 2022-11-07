5366461bec047cb1166c43899c4bb3e6eaf9e71d
Michael Gajardo <105249375+MikeGajardo@users.noreply.github.com>
2022-11-07 18:57:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var list = ul.getElementsByTagName("li")
+var li = document.querySelector("li");
+var list = ul.getElementsByTagName("li");
+var btn = document.createElement("button")
+btn.innerHTML= "Delete"
 
-console.log(list)
 
 function inputLength() {
 	return input.value.length;
@@ -12,6 +14,12 @@ function inputLength() {
 function createListElement() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
+	var deleteBtn = document.createElement("button")
+	deleteBtn.innerHTML = "Delete"
+	li.appendChild(deleteBtn);
+	deleteBtn.addEventListener("click", function(){
+		event.target.parentNode.remove();
+	})
 	ul.appendChild(li);
 	input.value = "";
 }
@@ -19,13 +27,13 @@ function createListElement() {
 function addListAfterClick() {
 	if (inputLength() > 0) {
 		createListElement();
-	}
+	} 
 }
 
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
-	}
+	} 
 }
 
 //Add Strikethrough to clicked li items
@@ -33,11 +41,21 @@ function addListAfterKeypress(event) {
 function addDoneAfterClick(event) {
 	if (event.target === event.target) {
 		event.target.classList.toggle("done");
-	}
+	} 
 } 
+
+//This deletes the list item by targeting the parent node
+
+function deleteButton() {
+	event.target.parentNode.remove();
+}
+
 
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
 
 ul.addEventListener("click", addDoneAfterClick);
+
+ul.addEventListener("click", deleteButton);
+
